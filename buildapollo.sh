8baaea14dca65c4e2371a5cd589d9ceb08074b15
Ben Lynch <31080607+blynchNDI@users.noreply.github.com>
2021-03-15 15:02:13 -0400

    Feat: use make docker to create container images

--- buildapollo.sh
+++ buildapollo.sh
@@ -4,4 +4,7 @@ cd /home/ubuntu/apollo/
 rm /home/ubuntu/apollo/settings.ini
 cp /home/ubuntu/settings.ini /home/ubuntu/apollo/settings.ini
 sudo docker rmi -f $(sudo docker images -a -q)
-sudo docker-compose up -d --force-recreate --renew-anon-volumes
+sudo git init
+sudo make docker
+sudo docker-compose down
+sudo docker-compose up -d
